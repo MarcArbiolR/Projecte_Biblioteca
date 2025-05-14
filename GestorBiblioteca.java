0ce1a30d392bcbb62767fd69af1f28d4d17c34e2
MarcA <marcarbiolrams@gmail.com>
2025-05-14 14:02:01 +0200

    Creació de atributs de la classe

--- GestorBiblioteca.java
+++ GestorBiblioteca.java
@@ -3,5 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GestorBiblioteca {
-
+    /* --- ATRIBUTS DE LA CLASSE --- */
+    // List<Prestec> prestecs: conté la llista de préstecs realitzats
+    private List<Prestec> prestecs;
 }
