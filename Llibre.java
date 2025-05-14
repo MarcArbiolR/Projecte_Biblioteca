f0a265dd1e1fe8ac23531d3f1b38eb8b5851c828
MarcA <marcarbiolrams@gmail.com>
2025-05-14 13:42:19 +0200

    Creacio dels atributs de la classe

---
+++ Llibre.java
@@ -0,0 +1,10 @@
+public class Llibre {
+    /* --- ATRIBUTS DE LA CLASSE --- */
+        // String titol: conté el títol del llibre
+        private String titol;
+        // String autor: conté el nom de l'autor del llibre
+        private String autor;
+        // boolean prestat: indica si el llibre està prestat o no
+        private boolean prestat;
+
+}
