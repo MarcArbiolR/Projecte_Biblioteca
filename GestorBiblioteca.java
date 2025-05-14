be52ba6761ac1bb91ad2ab3d8afa57658302b7b2
MarcA <marcarbiolrams@gmail.com>
2025-05-14 14:02:50 +0200

    Creacio de métodes.

--- GestorBiblioteca.java
+++ GestorBiblioteca.java
@@ -12,4 +12,17 @@ public class GestorBiblioteca {
         this.prestecs = new ArrayList<>();
     }
 
+    /* --- MÈTODES DE LA CLASSE --- */
+    // Mètode que afegeix un llibre a la llista de préstecs realitzats
+    public void prestarLlibre(Usuari usuari, Llibre llibre) {
+        if (!llibre.esPrestat()) {
+            llibre.prestar();
+            Prestec prestec = new Prestec(usuari, llibre, LocalDate.now());
+            prestecs.add(prestec);
+            usuari.afegirLlibre(llibre);
+            System.out.println(usuari.getNom() + " ha agafat el llibre: " + llibre.getTitol());
+        } else {
+            System.out.println("Aquest llibre ja està prestat.");
+        }
+    }
 }
