5266f776afda43292aa84a06a5743539c4007318
raulnavarro <raul.navarro1@insbaixcamp.cat>
2025-05-14 14:44:08 +0200

    Creació dels mètodes de la classe

--- Biblioteca.java
+++ Biblioteca.java
@@ -2,13 +2,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Biblioteca {
-    
+
     // --- ATRIBUTS ---
     private List<Llibre> llibres;
 
     // Constructor per la biblioteca
     public Biblioteca() {
         this.llibres = new ArrayList<>();
+    }
+
+    // ---- MÈTODES ----
+    // Mètode per afegir un llibre
+    public void afegirLlibre(Llibre llibre) { 
+        llibres.add(llibre); 
+    }
+
+    // Mètode per buscar un llibre a través del títol
+    public Llibre buscarLlibre(String titol) {
+        for (Llibre llibre : llibres) {
+            if (llibre.getTitol().equalsIgnoreCase(titol)) {
+            return llibre;
+            }
+        }
+        return null;
+    }
+
+    // Mètode per llistar llibres
+    public List<Llibre> getLlibres() { 
+        return llibres; 
     }
 
 }
