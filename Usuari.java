39d001f5b40ded893f168ab24588c75b36173abb
Alejandro <alejandro.sopenya@insbaixcamp.cat>
2025-05-16 12:42:59 +0200

    Agregamos los getters, setters y toString

--- Usuari.java
+++ Usuari.java
@@ -4,5 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Usuari {
+    private String nom;
+    private List<Llibre> llibresPrestats;
 
+    public Usuari(String nom) {
+        this.nom = nom;
+        this.llibresPrestats = new ArrayList<>();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuari: " + nom + " | Llibres en préstec: " + llibresPrestats.size();
+    }
 }
